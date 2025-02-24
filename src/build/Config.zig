3d2b0b6d854fd1e11c83258d55e580f683c9e2ae
waltari <gitting.crew@gmail.com>
2025-02-24 20:51:41 +0900

    custom

--- src/build/Config.zig
+++ src/build/Config.zig
@@ -192,26 +192,6 @@ pub fn init(b: *std.Build) !Config {
 
             else => return err,
         };
-        if (vsn.tag) |tag| {
-            // Tip releases behave just like any other pre-release so we skip.
-            if (!std.mem.eql(u8, tag, "tip")) {
-                const expected = b.fmt("v{d}.{d}.{d}", .{
-                    app_version.major,
-                    app_version.minor,
-                    app_version.patch,
-                });
-
-                if (!std.mem.eql(u8, tag, expected)) {
-                    @panic("tagged releases must be in vX.Y.Z format matching build.zig");
-                }
-
-                break :version .{
-                    .major = app_version.major,
-                    .minor = app_version.minor,
-                    .patch = app_version.patch,
-                };
-            }
-        }
 
         break :version .{
             .major = app_version.major,
